d8acefd914d1e1efcdd76e138d06cba14a17d78f
Franziska Hinkelmann <franzih@chromium.org>
2018-04-27 18:02:57 -0400

    Format

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// [START compute_engine_quickstart]
 // Imports the Google Cloud client library
 const Compute = require('@google-cloud/compute');
 const http = require('http');
@@ -18,8 +17,8 @@ const config = {
   metadata: {
     items: [
       {
-       key: "startup-script",
-       value: "#! /bin/bash\n\n# Installs apache and a custom homepage\napt-get update\napt-get install -y apache2\ncat <<EOF > /var/www/html/index.html\n<html><body><h1>Hello World</h1>\n<p>This page was created from a simple start up script!</p>\n</body></html>"
+        key: "startup-script",
+        value: "#! /bin/bash\n\n# Installs apache and a custom homepage\napt-get update\napt-get install -y apache2\ncat <<EOF > /var/www/html/index.html\n<html><body><h1>Hello World</h1>\n<p>This page was created from a simple start up script!</p>\n</body></html>"
       }
     ]
   }
@@ -39,28 +38,28 @@ zone
         console.log(name + ' created, running at ' + ip);
         console.log('Waiting for startup...')
 
-        const runPings = setInterval( (ip) => {
+        const runPings = setInterval((ip) => {
           http.get(ip, res => {
-            const {statusCode} = res
+            const { statusCode } = res
             if (statusCode === 200) {
               clearTimeout(runPings);
               console.log("Ready!");
             }
-       
+
           }).on('error', () => process.stdout.write("."))
-        }, 500, 'http://' + ip) 
+        }, 500, 'http://' + ip)
       })
-      .catch(err => console.error(err))
+        .catch(err => console.error(err))
     })
   })
-  .catch(err => console.error( err))
+  .catch(err => console.error(err))
 
- // List all VMs in that zone. 
- zone.getVMs()
- .then(data => {
-   const vms = data[0];
-   vms.forEach(vm => {
-     vm.getMetadata().then(data => console.log(vm.name + ": " + data[0]['networkInterfaces'][0]['accessConfigs'][0]['natIP']))
+// List all VMs in that zone. 
+zone.getVMs()
+  .then(data => {
+    const vms = data[0];
+    vms.forEach(vm => {
+      vm.getMetadata().then(data => console.log(vm.name + ": " + data[0]['networkInterfaces'][0]['accessConfigs'][0]['natIP']))
     })
- })
- .catch(err => console.error(err))
+  })
+  .catch(err => console.error(err))
